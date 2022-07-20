3920d84c53dc7ec57ba2a8fcc2f56c40a2e13d47
Li Xin <354010962@qq.com>
2022-07-20 17:29:48 +0800

    feat: support to hidden plugins

--- hango-api-plane-server/src/main/java/org/hango/cloud/web/controller/GatewayPluginController.java
+++ hango-api-plane-server/src/main/java/org/hango/cloud/web/controller/GatewayPluginController.java
@@ -1,19 +1,21 @@
 package org.hango.cloud.web.controller;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
 import org.hango.cloud.core.editor.ResourceGenerator;
 import org.hango.cloud.meta.Plugin;
 import org.hango.cloud.service.PluginService;
 import org.hango.cloud.util.errorcode.ApiPlaneErrorCode;
 import org.hango.cloud.util.errorcode.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.annotation.PostConstruct;
+import java.util.*;
 
 
 /**
@@ -26,6 +28,24 @@ public class GatewayPluginController extends BaseController {
 
     @Autowired
     private PluginService pluginService;
+
+    private Set<String> ignorePluginSet;
+
+    @Value("${ignorePlugins:#{null}}")
+    private String ignorePlugins;
+
+    public String getIgnorePlugins() {
+        return ignorePlugins;
+    }
+
+    public void setIgnorePlugins(String ignorePlugins) {
+        this.ignorePlugins = ignorePlugins;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.ignorePluginSet = StringUtils.isEmpty(ignorePlugins) ? Collections.emptySet() : new HashSet<>(Arrays.asList(ignorePlugins.split(",")));
+    }
 
     @RequestMapping(params = "Action=GetPluginDetail", method = RequestMethod.GET)
     public String getTemplate(@RequestParam("Name") String name) {
@@ -42,6 +62,9 @@ public class GatewayPluginController extends BaseController {
     @RequestMapping(params = "Action=GetPluginList", method = RequestMethod.GET)
     public String getPlugins() {
         Map<String, Plugin> plugins = pluginService.getPlugins();
+        for (String pluginName : this.ignorePluginSet) {
+            plugins.remove(pluginName);
+        }
         ErrorCode code = ApiPlaneErrorCode.Success;
         return apiReturn(code.getStatusCode(), code.getCode(), code.getMessage(), ImmutableMap.of("Plugins", plugins.values()));
     }
