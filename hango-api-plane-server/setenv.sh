4eb6c051eacbdff63c99e4aa44ec4c788ce53b81
yutao04 <yutao04@corp.netease.com>
2023-03-20 13:45:48 +0800

    Update Dockerfile for base commands(like netstat)
    Signed-off-by: yutao04 <yutao04@corp.netease.com>

--- hango-api-plane-server/setenv.sh
+++ hango-api-plane-server/setenv.sh
@@ -17,6 +17,8 @@ NCE_PERM=256m
 fi
 
 NCE_APPNAME=
-export NCE_JAVA_OPTS="$NCE_JAVA_OPTS -Xms${NCE_XMS} -Xmx${NCE_XMX} -XX:MaxPermSize=${NCE_PERM} -Dlog.dir=${CATALINA_HOME}/logs"
+export NCE_JAVA_OPTS="$NCE_JAVA_OPTS -Dlog.dir=${CATALINA_HOME}/logs"
+## 有需要可以按照如下配置案例添加堆内存 元空间等 内存配置
+## export NCE_JAVA_OPTS="$NCE_JAVA_OPTS -Xms${NCE_XMS} -Xmx${NCE_XMX} -XX:MaxPermSize=${NCE_PERM} -Dlog.dir=${CATALINA_HOME}/logs"
 
 export JAVA_OPTS="-Djava.awt.headless=true -server -verbose:gc -Djava.security.egd=file:/dev/./urandom -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -Xloggc:$CATALINA_HOME/logs/gc.log -XX:+PrintGCTimeStamps -Dforeign.domain=true -Dsun.rmi.transport.tcp.responseTimeout=20000 -Dsun.rmi.dgc.client.gcInterval=7200000 -Dsun.rmi.dgc.server.gcInterval=7200000 -Duser.timezone=Asia/Shanghai -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 $JAVA_OPTS $NCE_JAVA_OPTS"
