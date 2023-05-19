fd27a3940df4800b906258f44cac389ed0e3207b
yutao04 <yutao04@corp.netease.com>
2023-05-19 16:52:34 +0800

    Update for opensource files

--- hango-api-plane-server/setenv.sh
+++ hango-api-plane-server/setenv.sh
@@ -5,18 +5,19 @@ sed -i 's/\(<Connector port="\)[0-9]\{1,5\}\(" protocol="HTTP\/1.1"\)/\1'"$NCE_P
 fi
 
 if [ -z "$NCE_XMS" ]; then
-NCE_XMS=1024m
+NCE_XMS=512m
 fi
 
 if [ -z "$NCE_XMX" ]; then
-NCE_XMX=2048m
+NCE_XMX=512m
 fi
 
 if [ -z "$NCE_PERM" ]; then
 NCE_PERM=256m
 fi
 
-NCE_APPNAME=
-export NCE_JAVA_OPTS="$NCE_JAVA_OPTS -Xms${NCE_XMS} -Xmx${NCE_XMX} -XX:MaxPermSize=${NCE_PERM} -Dlog.dir=${CATALINA_HOME}/logs"
+export NCE_JAVA_OPTS="$NCE_JAVA_OPTS  -Xms${NCE_XMS} -Xmx${NCE_XMX} -Dlog.dir=${CATALINA_HOME}/logs"
+## 有需要可以按照如下配置案例添加堆内存 元空间等 内存配置
+## export NCE_JAVA_OPTS="$NCE_JAVA_OPTS -Xms${NCE_XMS} -Xmx${NCE_XMX} -XX:MaxPermSize=${NCE_PERM} -Dlog.dir=${CATALINA_HOME}/logs"
 
 export JAVA_OPTS="-Djava.awt.headless=true -server -verbose:gc -Djava.security.egd=file:/dev/./urandom -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -Xloggc:$CATALINA_HOME/logs/gc.log -XX:+PrintGCTimeStamps -Dforeign.domain=true -Dsun.rmi.transport.tcp.responseTimeout=20000 -Dsun.rmi.dgc.client.gcInterval=7200000 -Dsun.rmi.dgc.server.gcInterval=7200000 -Duser.timezone=Asia/Shanghai -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 $JAVA_OPTS $NCE_JAVA_OPTS"
